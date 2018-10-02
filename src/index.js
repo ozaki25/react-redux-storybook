c8d542623c50d5598ea5210320e0fd20b565c834
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-03 07:25:55 +0900

    remove .css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
 import App from 'src/App';
+
+// eslint-disable-next-line no-unused-expressions
+createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
