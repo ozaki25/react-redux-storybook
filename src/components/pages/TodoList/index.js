b73aeb696f69a923d6b713417448df991d14f28c
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-04 07:24:49 +0900

    add todolist page

---
+++ src/components/pages/TodoList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from 'src/components/utils/Container';
+
+const TodoList = () => (
+  <Container>
+    <p>TODO</p>
+  </Container>
+);
+
+export default TodoList;
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Hello from 'src/components/pages/Hello';
+import TodoList from 'src/components/pages/TodoList';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Hello} />
       <Route path="/hello" exact component={Hello} />
+      <Route path="/todolist" exact component={TodoList} />
     </Switch>
   </BrowserRouter>
 );
