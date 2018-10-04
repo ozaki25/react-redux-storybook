0d2769e06e563c7e8f177025e6ed26253847a961
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-05 07:47:07 +0900

    modify template

--- templates/atoms/index.stories.js
+++ templates/atoms/index.stories.js
@@ -4,7 +4,7 @@ import { text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import COMPONENT_NAME from '.';
 
-const stories = storiesOf(`atoms/${COMPONENT_NAME}`, module);
+const stories = storiesOf('atoms/COMPONENT_NAME', module);
 
 stories.add('COMPONENT_NAME', () => (
   <COMPONENT_NAME onClick={action('click')}>{text('text', 'text')}</COMPONENT_NAME>
