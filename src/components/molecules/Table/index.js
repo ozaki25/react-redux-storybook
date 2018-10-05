2811cd4112db8ccda249cde2271b99290fd41f20
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-05 19:03:50 +0900

    modify table component

--- src/components/molecules/Table/index.js
+++ src/components/molecules/Table/index.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Button from 'src/components/atoms/Button';
 
 const StyledTable = styled.table`
-  border: 1px solid #bbb;
-  border-radius: 0.1rem;
+  border-collapse: collapse;
   width: 100%;
+`;
+
+const StyledTh = styled.th`
+  border-bottom: 1px solid #bbb;
+  border-top: 1px solid #bbb;
+  padding: 0.75rem;
+  text-align: left;
+  vertical-align: top;
+`;
+
+const StyledTd = styled.td`
+  border-bottom: 1px solid #bbb;
+  border-top: 1px solid #bbb;
+  padding: 0.75rem;
+  text-align: left;
+  vertical-align: top;
 `;
 
 const Table = ({ header, body }) => (
   <StyledTable>
     <thead>
-      {header.map((th, key) => (
-        <th key={key}>{th}</th>
-      ))}
+      <tr>
+        {header.map((th, key) => (
+          <StyledTh key={key}>{th}</StyledTh>
+        ))}
+        <StyledTh />
+        <StyledTh />
+      </tr>
     </thead>
     <tbody>
       {body.map((tr, key) => (
         <tr key={key}>
           {tr.map((td, i) => (
-            <td key={i}>{td}</td>
+            <StyledTd key={i}>{td}</StyledTd>
           ))}
+          <StyledTd>
+            <Button>編集</Button>
+          </StyledTd>
+          <StyledTd>
+            <Button>削除</Button>
+          </StyledTd>
         </tr>
       ))}
     </tbody>
