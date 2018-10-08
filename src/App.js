451ac81df105966d52bd43e6ccb6e37831418407
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-09 08:36:24 +0900

    apply redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import Router from 'src/router/';
+import store from 'src/modules/createStore';
 
-const App = () => <Router />;
+const App = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
 
 export default App;
