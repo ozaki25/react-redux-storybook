32849a5b4c3fe1ba8d2052cf698711788b85a154
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-11 07:43:09 +0900

    update button style

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background-color: #fff;
-  border: 1px solid #bdbdbd;
-  border-radius: 0.25rem;
+  background-color: #f5f5f5;
+  border: none;
+  border-radius: 2px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  cursor: pointer;
   display: ${({ wide }) => (wide ? 'block' : 'inline-block')};
+  margin: 0;
+  outline: none;
   padding: 0.375rem 0.75rem;
   text-align: center;
-  user-select: none;
   vertical-align: middle;
   width: ${({ wide }) => (wide ? '100%' : 'auto')};
   :active {
-    opacity: 0.6;
+    opacity: 0.4;
+  }
+  :disabled {
+    cursor: not-allowed;
   }
 `;
 
