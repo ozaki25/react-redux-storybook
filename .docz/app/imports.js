fded7cbec69d6a3f8b2d1fe51ed759b0a1ee1e9e
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-10-11 09:35:59 +0900

    update gitignore

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/components/molecules/Table/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-molecules-table-index" */ 'src/components/molecules/Table/index.mdx'),
-  'src/components/atoms/Button/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-atoms-button-index" */ 'src/components/atoms/Button/index.mdx'),
-}
